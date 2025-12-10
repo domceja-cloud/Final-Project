ecf0f582ba57827a22cc1fe3b865dc1fa7a0d92e
domceja-cloud <domceja@berkeley.edu>
2025-12-09 16:27:37 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZG9taW5pY2NlamEiLCJhIjoiY21oOXUzc2tkMWZqdDJpbjF1d2d4azRpaSJ9.RBg-rbXZSK9Sq3XAGTgmgw';
+
+const map = new mapboxgl.Map({
+  container: 'map', // this is the container ID that we set in the HTML
+  center: [-122.27, 37.8], // starting position [lng, lat]. Note that lat must be set between -90 and 90. You can choose what you'd like.
+  zoom: 9 // starting zoom, again you can choose the level you'd like.
+    });
