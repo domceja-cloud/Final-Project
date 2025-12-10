a9a700c8dad19d316d4dd57a07ee6ed3bfc9c916
domceja-cloud <domceja@berkeley.edu>
2025-12-09 16:29:50 -0800

    Update script.js for style.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZG9taW5pY2NlamEiLCJhIjoiY21oOXUzc2tkMWZqdDJpb
 
 const map = new mapboxgl.Map({
   container: 'map', // this is the container ID that we set in the HTML
+  style: 'mapbox://styles/dominicceja/cmiz4afrj004c01r8ebol6gp7', // Your Style URL goes here
   center: [-122.27, 37.8], // starting position [lng, lat]. Note that lat must be set between -90 and 90. You can choose what you'd like.
   zoom: 9 // starting zoom, again you can choose the level you'd like.
     });
